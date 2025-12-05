7498a1b6d2a6bb4e419c51a7544952c2c76edce2
Kushon <mariakochenckova@gmail.com>
2025-12-05 15:47:46 +0300

    fixed missing import

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
-from ml import load_model_and_tokenizer
+from ml import load_model_and_tokenizer, predict
 
 
 class Input(BaseModel):
