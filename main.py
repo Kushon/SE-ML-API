e24a5cd5313ce7c31c7b293e2abeb74eb1cb2df7
Kushon <mariakochenckova@gmail.com>
2025-12-05 15:26:45 +0300

    clean code

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
-import torch
 from pydantic import BaseModel
-from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer, PreTrainedModel, PretrainedConfig
-import torch.nn as nn
+
+from ml import load_model_and_tokenizer
+
 
 class Input(BaseModel):
     comments: list[str]
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.model, app.state.tokenizer, app.state.device = load_model_and_tokenizer()
     yield
-    
 
 
 app = FastAPI(lifespan=lifespan)
@@ -20,9 +20,11 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/process")
 async def process_data(input: Input, request: Request):
-    tokenizer, model, device = request.app.state.tokenizer, request.app.state.model, request.app.state.device
+    tokenizer, model, device = (
+        request.app.state.tokenizer,
+        request.app.state.model,
+        request.app.state.device,
+    )
     texts = input.comments
     predictions = predict(tokenizer, model, device, texts)
     return {"predictions": predictions}
-
-
